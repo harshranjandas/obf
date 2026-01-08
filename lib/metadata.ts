db40ac7df9cfb8e169fb28bb9bc9a4e68c420c46
Arun Maddheshia <arun.maddheshia@gmail.com>
2026-01-08 14:58:17 +0530

    fix: update meta tags

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,24 +1,24 @@
+const appName = 'One Big Future | India’s Future as Imagined by Our Leaders';
+const appDescription =
+  'One Big Future is a platform featuring leaders, innovators, and policymakers sharing bold ideas shaping India in the 21st century through interviews, podcasts, and events.';
 export const appMetadata = {
-  title: 'One Big Future',
-  description:
-    'One Big Future is a venture studio that partners with founders to build high-growth, category-defining companies.',
+  title: appName,
+  description: appDescription,
   icons: {
     icon: [{ url: '/favicon.ico', sizes: 'any' }],
     apple: [{ url: '/apple-icon.png', sizes: '180x180', type: 'image/png' }],
   },
   openGraph: {
-    title: 'One Big Future',
-    description:
-      'One Big Future is a venture studio that partners with founders to build high-growth, category-defining companies.',
+    title: appName,
+    description: appDescription,
     type: 'website',
-    siteName: 'One Big Future',
+    siteName: appName,
     images: ['/og-image.jpg'],
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'One Big Future',
-    description:
-      'One Big Future is a venture studio that partners with founders to build high-growth, category-defining companies.',
+    title: appName,
+    description: appDescription,
     images: ['/og-image.jpg'],
   },
 };
